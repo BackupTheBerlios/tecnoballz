c4f5b96280824a7dd753d9b4e9f333534c68a4c9
gurumeditation <gurumeditation>
2007-02-01 20:24:54 +0000

    modify clear in offscreen_surface

--- tecnoballz/include/offscreen_surface.h
+++ tecnoballz/include/offscreen_surface.h
@@ -5,11 +5,11 @@
  * @date 2007-02
  * @copyright 1991-2007 TLK Games
  * @author Bruno Ethvignot
- * @version $Revision: 1.4 $
+ * @version $Revision: 1.5 $
  */
 /* 
  * copyright (c) 1991-2007 TLK Games all rights reserved
- * $Id: offscreen_surface.h,v 1.4 2007/02/01 13:24:22 gurumeditation Exp $
+ * $Id: offscreen_surface.h,v 1.5 2007/02/01 20:24:54 gurumeditation Exp $
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -39,6 +39,8 @@ private:
 public:
   offscreen_surface (Uint32 width, Uint32 height, Uint32 depth, Uint32 voffset = 0);
   ~offscreen_surface ();
+  void clear (Uint32 color = 0);
+  void clear (Uint32 color, Uint32 xcoord, Uint32 ycoord, Uint32 w, Uint32 height);
   void blit_surface (offscreen_surface *offscreen);
   void blit_surface (offscreen_surface *offscreen, Uint32 xcoord, Uint32 ycoord, Uint32 width, Uint32 height);
 };
--- tecnoballz/src/offscreen_surface.cc
+++ tecnoballz/src/offscreen_surface.cc
@@ -2,14 +2,14 @@
  * @file offscreen_surface.cc 
  * @brief an offscreen drawing surface
  * @created 2007-01-31
- * @date 2007-01-31
+ * @date 2007-02-01
  * @copyright 1991-2007 TLK Games
  * @author Bruno Ethvignot
- * @version $Revision: 1.4 $
+ * @version $Revision: 1.5 $
  */
 /* 
  * copyright (c) 1991-2007 TLK Games all rights reserved
- * $Id: offscreen_surface.cc,v 1.4 2007/02/01 13:24:22 gurumeditation Exp $
+ * $Id: offscreen_surface.cc,v 1.5 2007/02/01 20:24:54 gurumeditation Exp $
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -36,12 +36,15 @@
  * @param depth number of bits per pixel: 8, 16, or 24
  * @param voffset 
  */
-offscreen_surface::offscreen_surface (Uint32 w, Uint32 h, Uint32 depth, Uint32 voffset)
+offscreen_surface::offscreen_surface (Uint32 w, Uint32 h, Uint32 depth,
+                                      Uint32 voffset)
 {
-  std::cout << "offscreen_surface() w=" << w << " h=" << h << " d=" << depth << std::endl; 
+  std::
+    cout << "offscreen_surface() w=" << w << " h=" << h << " d=" << depth <<
+    std::endl;
   vertical_offset = voffset;
   create_surface (w, h, depth);
-  pixel_data = (char *) surface->pixels + surface->pitch * vertical_offset; 
+  pixel_data = (char *) surface->pixels + surface->pitch * vertical_offset;
 }
 
 /**
@@ -56,10 +59,11 @@ offscreen_surface::~offscreen_surface ()
  * @param offscreen pointer to a offscreen surface object
  */
 void
-offscreen_surface::blit_surface (offscreen_surface *offscreen)
+offscreen_surface::blit_surface (offscreen_surface * offscreen)
 {
-  SDL_Surface* surface_dest = offscreen->get_surface ();
-  SDL_Rect rect = {0, vertical_offset, surface->w, surface->h - vertical_offset};
+  SDL_Surface *surface_dest = offscreen->get_surface ();
+  SDL_Rect rect =
+    { 0, vertical_offset, surface->w, surface->h - vertical_offset };
   if (SDL_BlitSurface (surface, &rect, surface_dest, &rect) < 0)
     {
       std::cerr << "offscreen_surface::blit_surface() " <<
@@ -72,10 +76,11 @@ offscreen_surface::blit_surface (offscreen_surface *offscreen)
  * @param offscreen pointer to a offscreen surface object
  */
 void
-offscreen_surface::blit_surface (offscreen_surface *offscreen, Uint32 xcoord, Uint32 ycoord, Uint32 width, Uint32 height)
+offscreen_surface::blit_surface (offscreen_surface * offscreen, Uint32 xcoord,
+                                 Uint32 ycoord, Uint32 width, Uint32 height)
 {
-  SDL_Surface* surface_dest = offscreen->get_surface ();
-  SDL_Rect rect = {xcoord, ycoord + vertical_offset, width, height};
+  SDL_Surface *surface_dest = offscreen->get_surface ();
+  SDL_Rect rect = { xcoord, ycoord + vertical_offset, width, height };
   if (SDL_BlitSurface (surface, &rect, surface_dest, &rect) < 0)
     {
       std::cerr << "offscreen_surface::blit_surface() " <<
@@ -83,4 +88,32 @@ offscreen_surface::blit_surface (offscreen_surface *offscreen, Uint32 xcoord, Ui
     }
 }
 
+/**
+ * Clear surface
+ * @param color pixel color value
+ */
+void
+offscreen_surface::clear (Uint32 color)
+{
+  dynamic_cast < surface_sdl * >(this)->clear (color, 0, vertical_offset,
+                                               surface->w,
+                                               surface->h - vertical_offset);
+}
 
+/**
+ * Clear surface
+ * @param color pixel color value
+ * @param xcoord x coordinate in the surface
+ * @param ycoord y coordinate in the surface 
+ * @param w the width in pixels to clear
+ * @param h the height in pixels to clear
+ */
+void
+offscreen_surface::clear (Uint32 color, Uint32 xcoord, Uint32 ycoord,
+                          Uint32 w, Uint32 h)
+{
+  dynamic_cast < surface_sdl * >(this)->clear (color, xcoord,
+                                               ycoord + vertical_offset, w,
+                                               h);
+
+}
